7d0efb7416e67087c35a60711eaaf2ec59b089ba
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2025-01-30 22:27:36 +0100

    finish section 4 - useState, props

--- 4-animals/src/AnimalShow.js
+++ 4-animals/src/AnimalShow.js
@@ -1,5 +1,33 @@
-function AnimalShow() {
-  return <div>Cow!</div>;
+import "./AnimalShow.css";
+import { useState } from "react";
+import heart from "./svg/heart.svg";
+
+function AnimalShow({ type }) {
+  const [clicks, setClicks] = useState(0);
+  const imgStyle = {
+    padding: "10px",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100px",
+    height: "100px",
+  };
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
+  const image = `/src/svg/${type}.svg`;
+  return (
+    <div className="animal-show" onClick={handleClick}>
+      <img src={image} alt="animal" className="animal" />
+      <img
+        className="heart"
+        alt="heart"
+        src={heart}
+        style={{ width: 10 + 10 * clicks + "px" }}
+      />
+    </div>
+  );
 }
 
 export default AnimalShow;
--- 4-animals/src/App.js
+++ 4-animals/src/App.js
@@ -1,20 +1,30 @@
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+import "./App.css";
+
+function getRandomAnimal() {
+  const animalList = ["cow", "bird", "cat", "dog", "horse", "gator"];
+  const index = Math.floor(Math.random() * animalList.length);
+
+  return animalList[index];
+}
 
 function App() {
-  console.log("app.js");
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    console.log("count increment ", count);
-    setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
-  const [count, setCount] = useState(0);
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
 
   return (
-    <>
+    <div className="app">
       <button onClick={handleClick}>Add Animal</button>
-
-      <p>Animal count : {count}</p>
-    </>
+      <div className="animal-list">{renderedAnimals}</div>
+    </div>
   );
 }
 
