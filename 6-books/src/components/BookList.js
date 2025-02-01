85c8577c2493f7839be87ec20c89482b4e80637b
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2025-02-01 23:00:49 +0100

    add context provider

--- 6-books/src/components/BookList.js
+++ 6-books/src/components/BookList.js
@@ -1,11 +1,23 @@
 import BookShow from "./BookShow";
+import BooksContext from "../context/books";
+import { useContext } from "react";
 
 function BookList({ books, onDelete, onEdit }) {
+  const { count, incrementCount } = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
-    return <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit}/>;
+    return (
+      <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit} />
+    );
   });
 
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <>
+      {count}
+      <button onClick={incrementCount}>Increment</button>
+      <div className="book-list">{renderedBooks}</div>
+    </>
+  );
 }
 
 export default BookList;
